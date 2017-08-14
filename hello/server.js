f97ed06cdc37a13d6e9e1cbcd3c81ff220e351a0
Matt Horner <matt@matthorner.co.uk>
2017-08-14 15:57:03 +0100

    Update server.js

--- hello/server.js
+++ hello/server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
+app.get('/env', function (req, res) {
+  res.send('The ENV is set to' + process.env.ENV);
+});
+
 var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
 });
